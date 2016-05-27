1b1c0165930c2406fe3056b5955794c519b138dd
Beletskiy <Alexey1Beletskiy@gmail.com>
2016-05-27 15:04:04 +0300

    Add $scope.on method

--- app/js/components/list.js
+++ app/js/components/list.js
@@ -1,4 +1,5 @@
-function ListController(listFactory, cardFactory) {
+function ListController(listFactory, cardFactory, $scope) {
+    var self = this;
 
     this.arrOfCards = cardFactory.getCards(this.list);
 
@@ -7,17 +8,18 @@ function ListController(listFactory, cardFactory) {
     };
 
     this.getCards = function () {
+        console.log('getCards');
         return cardFactory.getCards(this.list);
     };
 
     this.deleteCard = function (card) {
         cardFactory.deleteCard(card);
-        this.arrOfCards = cardFactory.getCards(this.list);  //how to get rid this duplication ???
+        this.arrOfCards = cardFactory.getCards(this.list);
     };
 
     this.createCard = function () {
         cardFactory.createCard(this.list, this.cardDescription);
-        this.arrOfCards = cardFactory.getCards(this.list);   //how to get rid this duplication ???
+        this.arrOfCards = cardFactory.getCards(this.list);
         this.cardDescription = '';
     };
     
@@ -32,10 +34,13 @@ function ListController(listFactory, cardFactory) {
             cardFactory.changeSortIndicesInsideList(event.source, event.dest);
         },
         containment: '#lists-container'
-    }
+    };
+    $scope.$on('updateCard', function (event, updatedList) {
+        self.arrOfCards = cardFactory.getCards(self.list);
+    });
 }
-ListController.$inject = ['listFactory', 'cardFactory'];
-    app.component('trelloList', {
+ListController.$inject = ['listFactory', 'cardFactory', '$scope'];
+app.component('trelloList', {
     templateUrl: 'js/components/list.html',
     controller: ListController,
     bindings: {
